6875caaf5b8727529b5a8bdaaab9d7f6ee76c2fa
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-02-19 16:18:38 +0530

    added my first animation using useFrame

--- src/pages/FirstProject.jsx
+++ src/pages/FirstProject.jsx
@@ -1,6 +1,13 @@
 import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const FirstProject = () => {
+    const cubeRef = useRef();
+    useFrame((state, delta) => {
+        // console.log(delta)
+        cubeRef.current.rotation.y += delta
+    })
   return (
     <>
     <mesh position-x={-2} >
@@ -8,11 +15,13 @@ const FirstProject = () => {
       <meshBasicMaterial color="orange" />
       {/* <OrbitControls /> */}
     </mesh>
-    <mesh position-x={2} rotation-y={Math.PI * 0.25} scale={1.5} >
+
+    <mesh ref={ cubeRef } position-x={2} rotation-y={Math.PI * 0.25} scale={1.5} >
       <boxGeometry />
       <meshBasicMaterial color="mediumpurple" />
       <OrbitControls />
     </mesh>
+
     <mesh position-y={-1} rotation-x={ -Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />
