6fb38f839227f21d69d39755b7bfbc598cf4ed0b
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-02-19 16:41:08 +0530

    added my orbit controls manually

--- src/pages/FirstProject.jsx
+++ src/pages/FirstProject.jsx
@@ -1,31 +1,42 @@
-import { OrbitControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { useThree, extend, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 
+extend({ OrbitControls });
 const FirstProject = () => {
-    const cubeRef = useRef();
-    useFrame((state, delta) => {
-        // console.log(delta)
-        cubeRef.current.rotation.y += delta
-    })
+  const { camera, gl } = useThree()
+  const cubeRef = useRef();
+  const groupRef = useRef();
+  useFrame((state, delta) => {
+    // console.log(delta)
+    cubeRef.current.rotation.y += delta;
+    // groupRef.current.rotation.y += delta;
+  });
   return (
     <>
-    <mesh position-x={-2} >
-      <sphereGeometry />
-      <meshBasicMaterial color="orange" />
-      {/* <OrbitControls /> */}
-    </mesh>
+    <orbitControls args={[camera, gl.domElement]} />
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color="orange" />
+          {/* <OrbitControls /> */}
+        </mesh>
 
-    <mesh ref={ cubeRef } position-x={2} rotation-y={Math.PI * 0.25} scale={1.5} >
-      <boxGeometry />
-      <meshBasicMaterial color="mediumpurple" />
-      <OrbitControls />
-    </mesh>
-
-    <mesh position-y={-1} rotation-x={ -Math.PI * 0.5} scale={10}>
+        <mesh
+          ref={cubeRef}
+          position-x={2}
+          rotation-y={Math.PI * 0.25}
+          scale={1.5}
+        >
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+          {/* <OrbitControls /> */}
+        </mesh>
+      </group>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />
-    </mesh>
+      </mesh>
     </>
   );
 };
