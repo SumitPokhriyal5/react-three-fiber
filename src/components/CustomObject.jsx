80319b07d9f07e373e7c4737484e7bafa1c91dc7
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-02-22 15:11:20 +0530

    added buffer geometry in custom object component

--- src/components/CustomObject.jsx
+++ src/components/CustomObject.jsx
@@ -1,11 +1,21 @@
-
 const CustomObject = () => {
+  const verticesCount = 10 * 3;
+  const positions = new Float32Array(verticesCount * 3);
+  for (let i = 0; i < verticesCount * 3; i++)positions[i] = (Math.random() - 0.5) * 3;
+
   return (
     <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color={"red"} />
+      <bufferGeometry>
+        <bufferAttribute
+          attach="attributes-position"
+          count={verticesCount}
+          itemSize={3}
+          array={positions}
+        />
+      </bufferGeometry>
+      <meshBasicMaterial color={"red"} />
     </mesh>
-  )
-}
+  );
+};
 
-export default CustomObject
+export default CustomObject;
