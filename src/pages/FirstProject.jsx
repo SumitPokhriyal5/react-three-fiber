830f43244189c88c4ea9616e2a5dd368f7d7e0b0
Sumit Pokhriyal <sumitstan1998@gmail.com>
2024-02-26 12:22:43 +0530

    occlude to html added

--- src/pages/FirstProject.jsx
+++ src/pages/FirstProject.jsx
@@ -1,11 +1,18 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import CustomObject from "../components/CustomObject";
-import { OrbitControls, PivotControls, TransformControls } from "@react-three/drei";
+import {
+  Html,
+  OrbitControls,
+  PivotControls,
+  TransformControls,
+} from "@react-three/drei";
 
 const FirstProject = () => {
   const cubeRef = useRef();
   const groupRef = useRef();
+  const sphereRef = useRef();
+  const floorRef = useRef();
   useFrame((state, delta) => {
     // const angle = state.clock.elapsedTime;
     // state.camera.position.x = Math.sin(angle) * 8;
@@ -16,16 +23,19 @@ const FirstProject = () => {
   });
   return (
     <>
-    <OrbitControls makeDefault />
-    <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-    <ambientLight intensity={ 0.5 } />
+      <OrbitControls makeDefault />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
       <group ref={groupRef}>
-        <PivotControls anchor={ [0, 0, 0 ] } depthTest={false}>
-        <mesh position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color="orange" />
-          {/* <OrbitControls /> */}
-        </mesh>
+        <PivotControls anchor={[0, 0, 0]} depthTest={false}>
+          <mesh ref={sphereRef} position-x={-2}>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+            {/* <OrbitControls /> */}
+            <Html position={[1, 1, 0]} wrapperClass="label" center distanceFactor={ 6 } occlude={[sphereRef, cubeRef]}>
+              That is a Sphere 👍
+            </Html>
+          </mesh>
         </PivotControls>
         <mesh
           ref={cubeRef}
@@ -37,13 +47,18 @@ const FirstProject = () => {
           <meshStandardMaterial color="mediumpurple" />
           {/* <OrbitControls /> */}
         </mesh>
-      <TransformControls object={ cubeRef } />
+        <TransformControls object={cubeRef} />
       </group>
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+      <mesh
+        ref={floorRef}
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
       </mesh>
-      <CustomObject />
+      {/* <CustomObject /> */}
     </>
   );
 };
